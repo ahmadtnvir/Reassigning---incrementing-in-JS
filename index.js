b95dcd6fce05b4b0a773d1eeefaf27690093c8e6
Tanvir Ahmad <tnvir9@gmail.com>
2023-09-17 21:59:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let count = 5
+count = count + 1
+console.log(count)
+
+////*** Task ***/
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+// Decrease it down to 25, and then finally increase it to 70
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+// Console.log the value after each step
